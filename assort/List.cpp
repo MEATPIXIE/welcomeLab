336aca402ecd0c9f06aaa4e37216dfd0b2358976
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-10-12 11:48:08 -0700

    Update List.cpp

--- assort/List.cpp
+++ assort/List.cpp
@@ -3,24 +3,28 @@
 #include <iostream>
 using namespace std;
 
-List::List()
-    : str{ nullptr}
-{
-    str = new char[1];
-    str[0] = '\0';
+List::List(){
+    head = NULL;
 }
 
+/*
 List::List(const List& other){
     str = new char[strlen(other.str) + 1];
     strcpy(str, other.str);
     str[strlen(other.str)] = '\0';
 }
+*/
 
 List::List(List&& other){
-    str = other.str;
-    other.str = nullptr;
+    head = other.head;
+    other.head = NULL;
 }
 
 List::~List(){
-    delete str;
+    Node *temp = head;
+    while (temp != NULL){
+        Node *temp2 = temp->next;
+        delete temp;
+        temp = temp2;
+    }
 }
