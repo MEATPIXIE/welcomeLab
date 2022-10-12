73d311595f1d264ca06562d9ca3b8f3088ba4c40
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-10-12 12:18:40 -0700

    Update List.cpp

--- assort/List.cpp
+++ assort/List.cpp
@@ -1,6 +1,7 @@
 #include "List.h"
 #include <cstring>
 #include <iostream>
+#include <cstddef>
 using namespace std;
 
 List::List(){
@@ -28,3 +29,41 @@ List::~List(){
         temp = temp2;
     }
 }
+
+size_t List::count() const{
+    return 0;
+}
+
+void List::insert(const std::string& value){
+    cout << "ewe";
+}  
+
+
+const string& List::lookup(size_t index) const{
+    return NULL;
+}
+
+void List::print(bool reverse) const{
+    cout << "ewewe";
+}
+
+string List::remove(size_t index){
+    return 0;
+}
+
+size_t List::remove(const std::string& value){
+    return 0;
+}
+
+/*
+ostream& operator <<(ostream& ostr, const List& rhs){
+    for(int i = 0; i < rhs.mCount; i++){
+        ostr << rhs.mData[i] << " ";
+    }
+    ostr << " || ";
+    for(int i = rhs.mCount; i < rhs.mCapacity; i++){
+        ostr << rhs.mData[i] << " ";
+    }
+    return ostr;
+}
+*/
