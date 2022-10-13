ef9abadfa7ea260cbc642b889533d8b978e95df4
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-10-13 11:51:45 -0700

    Update List.cpp

--- assort/List.cpp
+++ assort/List.cpp
@@ -10,7 +10,7 @@ List::List(){
 
 List::List(const List& other){
     if (other.head == NULL){
-        return;
+        head = NULL;
     }
     Node* temp = other.head;
     Node* current = NULL;
@@ -45,30 +45,135 @@ List::~List(){
 }
 
 size_t List::count() const{
-    return 0;
+    if(head == NULL){
+        return 0;
+    }
+    Node* temp = head;
+    size_t yo = 0;
+    while(temp != NULL){
+        yo++;
+        temp = temp->next;
+    }
+    return yo;
 }
 
 void List::insert(const std::string& value){
-    cout << "ewe";
+    Node* temp = new Node;
+    temp->data = value;
+    temp->next = NULL;
+    if(head == NULL){
+        head = temp;
+    }else{
+        Node* current = head;
+        while(current->next != NULL){
+            current = current->next;
+        }
+        current->next = temp;
+    }
 }  
 
+//impliment index > nodes throw
 const string& List::lookup(size_t index) const{
+    int what = 0;
+    Node* temp = head;
+    while(temp != NULL){
+        if(what == index){
+            return (temp->data);
+        }
+        what++;
+        temp = temp->next;
+    }
     return NULL;
 }
 
-void List::print(bool reverse) const{
-    cout << "ewewe";
+void List::print( bool reverse) const{
+	if(reverse == false){
+		Node* node = this->head;
+		while (node != NULL) {
+        	cout<<node->data<<" ";
+			node = node->next;
+    	}
+	}else{
+		Node* node = this->head;
+		Node* current = node;
+    	Node *prev = NULL, *next = NULL;
+ 
+    	while (current != NULL) {
+        	next = current->next;
+        	current->next = prev;
+        	prev = current;
+        	current = next;
+    	}
+    	node = prev;
+		while (node != NULL) {
+        	cout<<node->data<<" ";
+			node = node->next;
+    	}
+	}
 }
 
 string List::remove(size_t index){
-    return 0;
+    if(head == NULL){
+        return NULL;
+    }
+    if(index == 0){
+        string mine = head->data;
+        delete head;
+        head = NULL;
+        return mine;
+    }
+    Node* current = head;
+    for (size_t i = 0; i < index - 1; i++) {
+       current = current->next;
+    }
+    string mine = current->next->data;
+    delete current->next;
+    current->next = NULL;
+    return mine;
 }
 
 size_t List::remove(const std::string& value){
-    return 0;
+    size_t yours = 0;
+    Node* current = head;
+    Node* prev = NULL;
+    while (current) {
+        if (current->data == value) {
+            yours++;
+            if (current == head) {
+                head = current->next;
+                delete current;
+                current = head;
+                continue;
+            }
+            prev->next = current->next;
+            delete current;
+            current = prev->next;
+        } else {
+            prev = current;
+            current = current->next;
+        }
+    }
+    return yours;
 }
 
 /*
+int main(){
+    cout << "hello" << endl;
+}
+
+void List::PushBack(int value){
+    if(head == NULL){
+        head = new List(value);
+    }
+    else{
+        List* Current = head;
+        while(Current->next != NULL){
+            Current = Current-next;
+        }
+        List Node* Temp = new List()
+    }
+}
+
 ostream& operator <<(ostream& ostr, const List& rhs){
     for(int i = 0; i < rhs.mCount; i++){
         ostr << rhs.mData[i] << " ";
