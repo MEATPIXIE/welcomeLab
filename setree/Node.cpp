bd3aad123cfe3958962ec3927bd7b4234c7eba14
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-10-19 00:08:02 -0700

    Update Node.cpp

--- setree/Node.cpp
+++ setree/Node.cpp
@@ -1,1 +1,18 @@
 #include "Node.h"
+#include <iostream>
+using namespace std;
+
+/*
+Node(){
+    data = "";
+    left = NULL;
+    right = NULL;
+}
+*/
+
+
+int main(){
+    cout << "dcdcdc" << endl;
+    
+    return 0;
+}
