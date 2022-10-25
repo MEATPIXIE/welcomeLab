7412d25414503510e92dc9ec392c9e041ca2054e
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-10-25 16:09:36 -0700

    Update Set.cpp

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -1,7 +1,3 @@
-#include "Set.h"
-#include <iostream>
-using namespace std;
-
 Set::Set(){
     mRoot = NULL;
 }
@@ -31,22 +27,13 @@ Set::~Set(){
     mRoot = nullptr;
 }
 
-void Clear(Node* node){
-    if(node == NULL){
-        return;
-    }else{
-        Clear(node->left);
-        Clear(node->right);
-        delete node;
-	node = NULL;
-        return;
-    }
-}
-
 size_t Set::clear() {
-    size_t num = this->count();
-    Clear(mRoot);
-    mRoot = NULL;
+    size_t num = count();
+    if (mRoot == nullptr) {
+        return 0;
+    }
+    delete mRoot;
+    mRoot = nullptr;
     return num;
 }
 
