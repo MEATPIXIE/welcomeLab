c84b1e52bcca2abd981f4529caae015bbc379c8e
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-10-29 15:04:51 -0700

    Update Nodes.h

--- polish/Nodes.h
+++ polish/Nodes.h
@@ -2,11 +2,14 @@
 #define NODES_H
 #include "AST.h"
 
+/*
 class Node{
-    int key;
+    double key;
     Node* left;
     Node* right;
 };
+*/
+
 class Number: public AST{
     double data;
 public:
@@ -16,8 +19,7 @@ public:
 };
 
 
-
-class Add: public AST{
+class Add:public AST{
     public:
         AST* left;
         AST* right;
@@ -28,9 +30,60 @@ class Add: public AST{
         double value() const;
 };
 
+class Sub:public AST{
+    public:
+        AST* left;
+        AST* right;
+        Sub(AST*, AST*);
+        ~Sub();
+        std::string prefix() const;
+        std::string postfix() const = 0;
+        double value() const;
+};
 
+class Mul:public AST{
+    public:
+        AST* left;
+        AST* right;
+        Mul(AST*, AST*);
+        ~Mul();
+        std::string prefix() const;
+        std::string postfix() const = 0;
+        double value() const;
+};
 
+class Div:public AST{
+    public:
+        AST* left;
+        AST* right;
+        Div(AST*, AST*);
+        ~Div();
+        std::string prefix() const;
+        std::string postfix() const = 0;
+        double value() const;
+};
 
+class Rem:public AST{
+    public:
+        AST* left;
+        AST* right;
+        Rem(AST*, AST*);
+        ~Rem();
+        std::string prefix() const;
+        std::string postfix() const = 0;
+        double value() const;
+};
+
+class Neg:public AST{
+    public:
+        AST* left;
+        //AST* right;
+        Neg(AST*, AST*);
+        ~Neg();
+        std::string prefix() const;
+        std::string postfix() const = 0;
+        double value() const;
+};
 
 
 #endif
