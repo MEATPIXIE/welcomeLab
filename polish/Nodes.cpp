f1d738eecddb8a6a5af197c6db7f4dcbb3e0f567
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-10-29 15:04:28 -0700

    Update Nodes.cpp

--- polish/Nodes.cpp
+++ polish/Nodes.cpp
@@ -14,7 +14,6 @@ std::string postfix() {
 }
 
 
-
 Add::Add(AST*, AST*){
 }
 Add::~Add(){
@@ -29,4 +28,89 @@ std::string Add::prefix() const{
 }
 std::string Add::postfix() const{
     return left->postfix() + " " + right->postfix() + " /";
+}
+
+
+Sub::Sub(AST*, AST*){
+}
+Sub::~Sub(){
+    delete left;
+    delete right;
+}
+double Sub::value() const{
+    return (left->value() - right->value());
+}
+std::string Sub::prefix() const{
+    return left->prefix() + " " - right->prefix() + " /";
+}
+std::string Sub::postfix() const{
+    return left->postfix() + " " - right->postfix() + " /";
+}
+
+
+Mul::Mul(AST*, AST*){
+}
+Mul::~Mul(){
+    delete left;
+    delete right;
+}
+double Mul::value() const{
+    return (left->value() * right->value());
+}
+std::string Mul::prefix() const{
+    return left->prefix() + " " * right->prefix() + " /";
+}
+std::string Mul::postfix() const{
+    return left->postfix() + " " * right->postfix() + " /";
+}
+
+
+Div::Div(AST*, AST*){
+}
+Div::~Div(){
+    delete left;
+    delete right;
+}
+double Div::value() const{
+    return (left->value() / right->value());
+}
+std::string Div::prefix() const{
+    return left->prefix() + " " / right->prefix() + " /";
+}
+std::string Div::postfix() const{
+    return left->postfix() + " " / right->postfix() + " /";
+}
+
+
+Rem::Rem(AST*, AST*){
+}
+Rem::~Rem(){
+    delete left;
+    delete right;
+}
+double Rem::value() const{
+    return (left->value() % right->value());
+}
+std::string Rem::prefix() const{
+    return left->prefix() + " " % right->prefix() + " /";
+}
+std::string Rem::postfix() const{
+    return left->postfix() + " " % right->postfix() + " /";
+}
+
+
+Neg::Neg(AST*, AST*){
+}
+Neg::~Neg(){
+    delete left;
+    //delete right;
+}
+double Neg::value() const{
+    return (left->value() * -1);
+}
+std::string Neg::prefix() const{
+    return left->prefix() + " " * -1 + " /";
+}
+std::string Neg::postfix() const{
+    return left->postfix() + " " * -1 + " /";
 }
