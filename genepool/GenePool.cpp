963b5355683bc53eac4a73274e4f700b66dddd53
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-02 15:51:38 -0700

    Update GenePool.cpp

--- genepool/GenePool.cpp
+++ genepool/GenePool.cpp
@@ -1,3 +1,11 @@
 #include "GenePool.h"
 
-// GenePool Member Functions
+static map<Person*, Person> pool;
+
+GenePool::GenePool(std::istream& stream){
+}
+GenePool::~GenePool(){
+}
+Person* GenePool::find(const std::string& name) const{
+    return nullptr;
+}
