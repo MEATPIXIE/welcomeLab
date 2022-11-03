5415b57c871b64e6b93ec7a2e061c22e1f847519
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-03 15:29:06 -0700

    Update GenePool.cpp

--- genepool/GenePool.cpp
+++ genepool/GenePool.cpp
@@ -1,11 +1,30 @@
 #include "GenePool.h"
-
-//static map<Person*, Person> pool;
+//#include <string>
 
 GenePool::GenePool(std::istream& stream){
+    std::string line;
+    while(getline(stream, line)){
+        //istringstream ss(line);
+        if(line == "#" || line == "\n"){
+            continue;
+        }
+    }
 }
 GenePool::~GenePool(){
+    for(std::map<string, Person*>::iterator itr = pool.begin(); itr != pool.end(); itr++){
+        delete itr->second;
+    }
 }
 Person* GenePool::find(const std::string& name) const{
+    /*
+    for(std::map<string, Person*>::iterator itr = pool.begin(); itr != pool.end(); itr++){
+        if(pool[i].myName == name){
+            itr = pool.find(name);
+            return itr;
+        }else{
+            return nullptr;
+        }  
+    }
+    */
     return nullptr;
 }
