77b3f90f1a1cf3a5a9c470718248047ebe3cbc71
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-06 20:57:47 -0800

    Update Person.cpp

--- genepool/Person.cpp
+++ genepool/Person.cpp
@@ -2,19 +2,18 @@
 
 
 Person::Person(std::string name, Gender gender, Person* mother, Person* father){
-    myName = name;
-
-    /*
-    if(gender == "male"){
-        myGender = Gender::MALE;
-    }else if(gender == "female"){
-        myGender = Gender::FEMALE;
-    }
-    */
     
+    myName = name;
     myGender = gender;
     myMom = mother;
     myDad = father;
+    
+    if(myMom != NULL){
+        myMom->myKids.insert({this});
+    }else if(myDad != NULL){
+        myDad->myKids.insert({this});
+    }
+    
 }
 
 const std::string& Person::name() const{
@@ -46,12 +45,14 @@ std::set<Person*> Person::brothers(PMod pmod, SMod smod){
 }
 
 std::set<Person*> Person::children() {
-    return this->myKids;
+    return myKids;
 }
+
 std::set<Person*> Person::cousins(PMod pmod, SMod smod){
     std::set<Person*> empty = {};
     return empty;
 }
+
 std::set<Person*> Person::daughters(){
     std::set<Person*> daughter = {};
     std::set<Person*> child = children();
@@ -62,6 +63,7 @@ std::set<Person*> Person::daughters(){
     }
     return daughter;
 }
+
 std::set<Person*> Person::descendants(){
     std::set<Person*> empty = {};
     return empty;
@@ -98,10 +100,24 @@ std::set<Person*> Person::nieces(PMod pmod, SMod smod){
     std::set<Person*> empty = {};
     return empty;
 }
+
 std::set<Person*> Person::parents(PMod pmod){
-    std::set<Person*> empty = {};
-    return empty;
+    std::set<Person*> parents = {};
+    if (pmod == PMod::PATERNAL && myDad != NULL){
+        parents.insert({myDad});
+    }else if (pmod == PMod::MATERNAL && myMom != NULL){
+        parents.insert({myMom});
+    }else if (pmod == PMod::ANY){
+        if (myDad != NULL){
+            parents.insert({myDad});
+        }
+        if (myMom != NULL){
+            parents.insert({myMom});
+        }
+    }
+    return parents;
 }
+
 std::set<Person*> Person::siblings(PMod pmod, SMod smod){
     std::set<Person*> empty = {};
     return empty;
@@ -110,6 +126,7 @@ std::set<Person*> Person::sisters(PMod pmod, SMod smod){
     std::set<Person*> empty = {};
     return empty;
 }
+
 std::set<Person*> Person::sons(){
     std::set<Person*> son = {};
     std::set<Person*> child = children();
@@ -120,6 +137,7 @@ std::set<Person*> Person::sons(){
     }
     return son;
 }
+
 std::set<Person*> Person::uncles(PMod pmod, SMod smod){
     std::set<Person*> empty = {};
     return empty;
