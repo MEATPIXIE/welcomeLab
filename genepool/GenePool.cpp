5822980b3245bae0300cfc9d0133546fbcd64f7c
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-06 17:28:48 -0800

    Update GenePool.cpp

--- genepool/GenePool.cpp
+++ genepool/GenePool.cpp
@@ -1,12 +1,51 @@
+
 #include "GenePool.h"
+#include <sstream>
+using namespace std;
 
 GenePool::GenePool(std::istream& stream){
+    
     std::string line;
+    std::string nombre;
+    std::string genero;
+    std::string mamaSeLlama;
+    std::string papaSeLlama;
+    
+    Gender gender;
+    Person* mama;
+    Person* papa;
+
     while(getline(stream, line)){
-        //istringstream ss(line);
-        if(line == "#" || line == "\n"){
+        istringstream leer(line);
+        if(line[0] == '#' || line.empty()){
             continue;
+        }else{
+            getline(leer, nombre, '\t');
+            getline(leer, genero, '\t');
+            getline(leer, mamaSeLlama, '\t');
+            getline(leer, papaSeLlama, '\t');
         }
+        
+        if(genero == "male"){
+            gender = Gender::MALE;
+        }else if(genero == "female"){
+            gender = Gender::FEMALE;
+        }
+        
+        if(mamaSeLlama == "???"){
+            mama = NULL;
+        }else{
+            mama = find(mamaSeLlama);
+        }
+        if(papaSeLlama == "???"){
+            papa = NULL;
+        }else{
+            papa = find(papaSeLlama);
+        }
+        
+        Person* person = new Person(nombre, genero, mama, papa);
+        pool.insert({nombre, person});
+        
     }
 }
 GenePool::~GenePool(){
