4f05fac66b30e4eefb26522974e003e46e86447e
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-14 15:22:25 -0800

    Update StarMap.cpp

--- starmap/StarMap.cpp
+++ starmap/StarMap.cpp
@@ -1,7 +1,28 @@
+
 #include "StarMap.h"
+
+
+
+StarMap::StarMap(std::istream& stream){
+}
+
+StarMap::~StarMap(){
+    /*
+    for(std::map<std::string, Person*>::iterator itr = pool.begin(); itr != pool.end(); itr++){
+        delete itr->second;
+    }
+    */
+}
+
+
 
 StarMap* StarMap::create(std::istream& stream) {
   // This default implementation will probably do what you want.
   // If you use a different constructor, you'll need to change it.
   return new StarMap(stream);
+}
+
+std::vector<Star> StarMap::find(size_t n, float x, float y, float z){
+    std::vector<Star> mine;
+    return mine;
 }
