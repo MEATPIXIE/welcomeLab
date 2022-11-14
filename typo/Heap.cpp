b69ce42e0e49b0c3510a82b9ae9de8c55bd4721f
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-13 19:20:11 -0800

    Update Heap.cpp

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -85,7 +85,7 @@ Heap::Entry Heap::pop(){
     if (mCount == 0){
         throw underflow_error("UE");
     }else{
-        mData[0] = mData[mCount];
+        mData[0] = mData[mCount - 1];
         mCount--;
         
         size_t ours = 0;  
