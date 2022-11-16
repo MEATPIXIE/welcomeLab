a96d6b3543b2c5159eb49709937c71ecb3f8ed2b
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-16 15:26:08 -0800

    Update Helpers.h

--- starmap/Helpers.h
+++ starmap/Helpers.h
@@ -1,7 +1,37 @@
+
 #ifndef HELPERS_H
 #define HELPERS_H
 
-// If you want to add any helper classes,
-// here's some space to do it in.
+#include <cstddef>
+#include <string>
+
+class Heap {
+public:
+    struct Entry {
+        std::string value;
+        float       score;
+    };
+
+private:
+    Entry* mData;
+    size_t mCapacity;
+    size_t mCount;
+
+public:
+    Heap(size_t capacity);
+    Heap(const Heap& other);
+    Heap(Heap&& other);
+    ~Heap();
+
+    size_t       capacity() const;
+    size_t       count() const;
+    const Entry& lookup(size_t index) const;
+    Entry        pop();
+    Entry        pushpop(const std::string& value, float score);
+    void         push(const std::string& value, float score);
+    const Entry& top() const;
+};
+
+
 
 #endif
