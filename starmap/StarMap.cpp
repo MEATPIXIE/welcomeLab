14e72fcb3d03833a3e06e5f59be337472f9ee67c
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-18 14:19:07 -0800

    Update StarMap.cpp

--- starmap/StarMap.cpp
+++ starmap/StarMap.cpp
@@ -36,9 +36,9 @@ StarMap* StarMap::create(std::istream& stream) {
 std::vector<Star> StarMap::find(size_t n, float x, float y, float z){
     
     Heap distanceStars = Heap(n);
-    std::vector<Star> mine;
+    //std::vector<Star> mine;
     for(Star estrella : *theUniverse){
-        float distance = ((estrella.x - x) * (estrella.x - x)) + ((estrella.y - y) * (estrella.y - y)) + ((estrella.z - z) * (estrella.z - z));//distanceFrom(x, y, z);
+        float distance = sqrt(((estrella.x - x) * (estrella.x - x)) + ((estrella.y - y) * (estrella.y - y)) + ((estrella.z - z) * (estrella.z - z)));//distanceFrom(x, y, z);
         if (distanceStars.capacity() != distanceStars.count()){
             distanceStars.push(estrella, distance); 
         }else if (distance < distanceStars.top().distance){
