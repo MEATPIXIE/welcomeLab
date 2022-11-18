c9e4a7a05ee08d5d0d410d0904d462aca5a1bcf2
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-18 13:15:12 -0800

    Update Helpers.cpp

--- starmap/Helpers.cpp
+++ starmap/Helpers.cpp
@@ -1,229 +1,57 @@
 
+#include "StarMap.h"
 #include "Helpers.h"
 
-#include <stdexcept>
-#include <cmath>
-using namespace std;
 
+StarMap::StarMap(std::istream& stream){
+    theUniverse = new vector<Star>;
+    int count = 1;
+    double x, y, z;
 
-Heap::Heap (size_t capacity){
-    mCapacity = capacity;
-    mCount = 0;
-    mData = new Entry[capacity];
-}
-Heap::Heap (const Heap & other){
-    this->mCapacity = other.mCapacity;
-    this->mCount = other.mCount;
-    this->mData = other.mData;
-    Entry *mine = new Entry[mCapacity];
-    if (other.mData == NULL){
-        return;
-    }else{
-        for (size_t i = 0; i < mCount; i++){
-            mine[i] = other.mData[i];
-        }
-    }
-    mData = mine;
-}
-Heap::Heap (Heap && other){
-    this->mCapacity = other.mCapacity;
-    this->mCount = other.mCount;
-    this->mData = other.mData;
-    other.mData = NULL;
-}
-Heap::~Heap(){
-    delete[]mData;
-}
-size_t Heap::capacity() const{
-  return mCapacity;
-}
-size_t Heap::count() const{
-  return mCount;
-}
-const Heap::Entry& Heap::lookup (size_t index) const{
-    if (index >= mCount || mCount == 0){
-        throw out_of_range ("OoR");
-    }else{
-        return mData[index];
-    }
-}
-const Heap::Entry& Heap::top() const{
-    if (mCount == 0){
-        throw underflow_error("UE");
-    }else{
-        return mData[0];
+    while(stream >> x >> y >> z){
+        Star aStar;
+        
+        aStar.x = x;
+        aStar.y = y;
+        aStar.z = z;
+        aStar.id = count;
+        theUniverse->push_back(aStar);
+        
+        count++;
     }
 }
 
-void Heap::push(const Star& value, float distance){
-    Entry var = {value, distance};
-    if (mCount > mCapacity){
-        throw overflow_error("OE");
-    }else if(count() == capacity()){
-        throw overflow_error("OE");
-    }else{
-        size_t ours = mCount;
-        mData[ours] = var;
-        mCount++;
-        while(mData[(ours - 1)/2].distance > mData[ours].distance && ours != 0){
-            Entry theirs = mData[ours];
-            mData[ours] = mData[(ours - 1)/2];
-            mData[(ours - 1)/2] = theirs;
-            ours = (ours - 1)/2;
-        }
-    }
+StarMap::~StarMap(){
+    delete theUniverse;
 }
-Heap::Entry Heap::pop(){
-    if (mCount == 0){
-        throw underflow_error("UE");
-    }else if(mCount == 1){
-        mCount--;
-        return mData[0];
-    }
 
-    Entry var = mData[0];
-    mData[0] = mData[mCount - 1];
-    mCount--;
-    size_t ours = 0;   
+
+StarMap* StarMap::create(std::istream& stream) {
+  // This default implementation will probably do what you want.
+  // If you use a different constructor, you'll need to change it.
+  return new StarMap(stream);
+}
+
+
+std::vector<Star> StarMap::find(size_t n, float x, float y, float z){
     
-    while(ours < mCount){
-        size_t first = (ours * 2) + 1;
-        size_t second = (ours * 2) + 2;
-        size_t largest = ours;
-
-        if (first < mCount && mData[first].distance > mData[ours].distance){
-            largest = first;
-        }else{
-            largest = ours;
-        }
-        if (second < mCount && mData[second].distance > mData[ours].distance){
-            largest = second;
-        }
-        if (largest != ours){
-            swap(mData[ours], mData[largest]);
-            ours = largest;
-        }else{
-            break;
+    Heap distanceStars = Heap(n);
+    std::vector<Star> mine;
+    for(Star estrella : *theUniverse){
+        float distance = ((estrella.x - x) * (estrella.x - x)) + ((estrella.y - y) * (estrella.y - y)) + ((estrella.z - z) * (estrella.z - z));//distanceFrom(x, y, z);
+        if (distanceStars.capacity() != distanceStars.count()){
+            distanceStars.push(estrella, distance); 
+        }else if (distance < distanceStars.top().distance){
+            distanceStars.pushpop(estrella, distance);
         }
     }
-    return var;
-}
-Heap::Entry Heap::pushpop(const Star& value, float distance){
     
-    if (mCount == 0){
-        throw underflow_error("UE");
+    while(distanceStars.count() != 0){
+        mine.push_back(distanceStars.pop().star);
     }
+    reverse(mine.begin(), mine.end());
 
-    Entry var = mData[0];
-    mData[0] = {value, distance};
-    size_t ours = 0;   
-    
-    while(ours < mCount){
-        size_t first = ours * 2 + 1;
-        size_t second = ours * 2 + 2;
-        if (first >= mCount){
-            break;
-        }
-        if (second >= mCount){
-            if (mData[ours].distance < mData[first].distance){
-                Entry temp = mData[ours];
-                mData[ours] = mData[first];
-                mData[first] = temp;
-                ours = first;
-            }else{
-                break;
-            }
-        }
-        else{
-            if (mData[ours].distance < mData[first].distance && mData[first].distance >= mData[second].distance){
-                Entry temp = mData[ours];
-                mData[ours] = mData[first];
-                mData[first] = temp;
-                ours = first;
-            }else if (mData[ours].distance < mData[second].distance && mData[second].distance >= mData[first].distance){
-                Entry temp = mData[ours];
-                mData[ours] = mData[second];
-                mData[second] = temp;
-                ours = second;
-            }else{
-                break;
-            }
-        }
-    }
-    return var;
-}
-double distanceFrom(double x, double y, double z){
-    double firstJump = sqrt(((Star.x - x)*(Star.x - x)) + ((Star.y - y)*(Star.y - y)));
-    double secondJump = sqrt((firstJump * firstJump) + ((Star.z - z)*(Star.z - z)));
-    return secondJump;
-} 
-
-////////////////////////////////////////////////////////////////////////////////
-/////////////  HEAP ^  /////////////  K-DIMENTIONAL MAP v  /////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-/*
-const int k = 2 //3
-
-struct Node{
-    int point[k];
-    Node *left, *right;
-}
-struct Node* newNode(int arr[]){
-    struct Node* temp = new Node;
-
-    for (int i= 0; i< k; i++){
-        temp -> point[i] = arr[i];
-    }
-    temp->left = temp->right = NULL;
-    return temp;
+    return mine;
 }
 
-// Insert new point with given point in KD Tree and return new root
-Node *insertRec(Node *root, int point[], unsigned depth){
-    if (root == NULL){
-       return newNode(point);
-    }
-    unsigned cd = depth % k; //x, y, or z?
-  
-    // Compare and decide left or right subtree
-    if (point[cd] < (root->point[cd]))
-        root->left  = insertRec(root->left, point, depth + 1);
-    else
-        root->right = insertRec(root->right, point, depth + 1);
-  
-    return root;
-}
-Node* insert(Node *root, int point[]){
-    return insertRec(root, point, 0);
-}
 
-// Searches a Point represented by "point[]" in the K D tree.
-bool searchRec(Node* root, int point[], unsigned depth){
-    if (root == NULL)
-        return false;
-    if (arePointsSame(root->point, point))
-        return true;
-  
-    // Current dimension is computed using current depth and total dimensions (k)
-    unsigned cd = depth % k;
-  
-    // Compare point with root with respect to cd (Current dimension)
-    if (point[cd] < root->point[cd]){
-        return searchRec(root->left, point, depth + 1);
-    }
-    return searchRec(root->right, point, depth + 1);
-}
-bool search(Node* root, int point[]){
-    return searchRec(root, point, 0);//Pass current depth as 0
-}
-
-bool arePointsSame(int point1[], int point2[]){
-    for(int i = 0; i < k; i++){
-        if (point1[i] != point2[i]){
-            return false;
-        }
-    }
-    return true;
-}
-*/
