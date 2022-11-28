38cbb38e0889944c6015c6a550eb7580c682aabe
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-11-28 04:11:52 -0800

    Add files via upload

---
+++ omnibus/Atlas.cpp
@@ -0,0 +1,14 @@
+#include "Atlas.h"
+
+Atlas::Atlas(std::istream& stream){
+    int count = 0;
+}
+
+Atlas::~Atlas(){
+}
+
+Atlas* Atlas::create(std::istream& stream) {
+  // This default implementation will probably do what you want.
+  // If you use a different constructor, you'll need to change it.
+  return new Atlas(stream);
+}
