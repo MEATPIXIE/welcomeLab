81ff1fc31c9b868ee489b7334ec8ed0b52bf3d4b
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-12-07 00:24:35 -0800

    Update Dictionary.cpp

--- typo/Dictionary.cpp
+++ typo/Dictionary.cpp
@@ -1,11 +1,22 @@
 
 #include "Dictionary.h"
+#include <string>
 
 
 Dictionary::Dictionary(std::istream& stream){
+    std::string word;
+    while(getline(stream, word)){
+        for (size_t i = 0; i < word.length(); i++){
+            if(isupper(word[i])){
+                continue;
+            }
+        }
+        mWords.push_back(word);
+    }
     return;
 }
 
 Heap Dictionary::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const{
+    Heap scores = Heap(maxcount); 
     return 0;
 }
