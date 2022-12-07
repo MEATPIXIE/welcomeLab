96500118274e2e3e41ea93ebb6d8a7d846d74808
MEATPIXIE <91990311+MEATPIXIE@users.noreply.github.com>
2022-12-07 00:41:30 -0800

    Update Dictionary.cpp

--- typo/Dictionary.cpp
+++ typo/Dictionary.cpp
@@ -1,10 +1,11 @@
 
 #include "Dictionary.h"
 #include <string>
-
+#include <cmath>
+using namespace std;
 
 Dictionary::Dictionary(std::istream& stream){
-    std::string word;
+    string word;
     while(getline(stream, word)){
         for (size_t i = 0; i < word.length(); i++){
             if(isupper(word[i])){
@@ -17,6 +18,32 @@ Dictionary::Dictionary(std::istream& stream){
 }
 
 Heap Dictionary::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const{
-    Heap scores = Heap(maxcount); 
-    return 0;
+    
+    size_t size = points.size();
+    Heap scores = Heap(maxcount);
+    float score = 0;
+    float done = 0;
+    float average = 0;
+    
+    for (string word:mWords){
+        if (word.length() == size){
+            for (size_t i = 0; i < word.length(); i++){
+                if (islower(word[i])){
+                    done = sqrt(pow(points[i].x - QWERTY[word[i] - 'a'].x,2) + pow(points[i].y - QWERTY[word[i] - 'a'].y, 2));
+                    score = score + (1/((10*pow(done,2)) + 1));
+                }
+            }
+            
+            average = score/size;
+            
+            if(average > cutoff){
+                if (scores.count() != scores.capacity()){
+                    scores.push(word, average);
+                }else if(average > scores.top().score){
+                    scores.pushpop(word, average);
+                }
+            }
+        }
+    }
+    return scores;
 }
